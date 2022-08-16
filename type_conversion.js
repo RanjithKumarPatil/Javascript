fc03aa9be4d6e95b45c482815bca3eb42cc68dce
RanjithKumar <ranjithkumarpatil@gmail.com>
2022-08-16 10:36:29 +0530

    javascript practice

---
+++ type_conversion.js
@@ -0,0 +1,9 @@
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
---
+++ variables.js
@@ -0,0 +1,33 @@
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Engineer";
+
+let job1 = "programmer";
+let job2 = "engineer";
+
+console.log(myFirstJob);
+
+////////////////////////////////////
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Patil";
+console.log(lastName);
